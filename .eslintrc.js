a742fca019a6675d7fc25475504cf1c10c9eaf87
acorreagomes <acorreagomes@gmail.com>
2020-05-08 00:33:47 -0300

    login screen

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
     'prettier',
     'prettier/react'
@@ -21,16 +20,27 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier'
-  ],
+  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      { extensions: ['.jsx', '.js']}
-    ],
-    'import/prefer-default-export': 'off'
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["warn", {extensions: [".jsx", ".js"]}],
+    "import/prefer-default-export": "off",
+    "no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+    "react/jsx-one-expression-per-line": "off",
+    "global-require": "off",
+    "react-native/no-raw-text": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "no-console": ["error", {allow: ["tron"]}],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    "import/resolver": {
+     "babel-plugin-root-import": {
+       rootPathSuffix: "src"
+     },
+    },
   },
 };
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '~/assets/ticket2.png';
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <img src={Logo} alt="EasyControl" />
+      <form>
+        <text>Ola, Seja Bem-Vindo(a)</text>
+        <text>Entre com suas credenciais.</text>
+        <input type="email" placeholder="Digite seu e-mail" />
+        <input type="password" placeholder="Digite sua senha" />
+        <button type="submit">Entrar</button>
+        <Link to="/dashboard">Criar conta</Link>
+      </form>
+    </>
   );
 }
