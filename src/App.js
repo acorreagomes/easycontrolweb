d3e432d83f9ff0fadd454fb252ec279c2f7b8f2b
acorreagomes <33076416+acorreagomes@users.noreply.github.com>
2020-05-08 17:29:21 -0300

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import { Router } from 'react-router-dom';
 
@@ -7,15 +8,19 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+      </Router>
+    </Provider>
   );
 }
 
